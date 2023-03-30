9276d75f4b52f478e67c4ce7fd405f55148f326b
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-03-29 21:07:36 -0300

    update lyout

--- bmic.py
+++ bmic.py
@@ -54,8 +54,8 @@ def main_window(frm_main):
     lbl_height.grid(row=3, column=0)
     ent_height = FloatEntry(frm_main, width=15, lower_bound=60, upper_bound=2000)
     ent_height.grid(row=3, column=1)
-    # lbl_height_unit = Label(frm_main, text="cm")
-    # lbl_height_unit.grid(row=3, column=2)
+    lbl_height_unit = Label(frm_main, text="cm")
+    lbl_height_unit.grid(row=3, column=2)
 
     lbl_Weight = Label(frm_main, text="Weight" )
     lbl_Weight.grid(row=4, column=0)
@@ -64,9 +64,9 @@ def main_window(frm_main):
     lbl_Weight_unit = Label(frm_main, text="kg")
     lbl_Weight_unit.grid(row=4, column=2)
 
-    ent_height2 = FloatEntry(frm_main, width=0, lower_bound=60, upper_bound=2000)
+    ent_height2 = FloatEntry(frm_main, width=7, lower_bound=60, upper_bound=2000)
     ent_height2.grid(row=3, column=3)
-    lbl_height2_unit = Label(frm_main, text="")
+    lbl_height2_unit = Label(frm_main, text="inches")
     lbl_height2_unit.grid(row=3, column=4)
 
     lbl_result = Label(frm_main, text="----TEST_RESULT----")
@@ -82,7 +82,7 @@ def main_window(frm_main):
         """Clear all the inputs and outputs."""
         btn_clear.focus()
         ent_age.clear()
-        # ent_height.clear()
+        ent_height.clear()
         ent_Weight.clear()
         ent_age.focus()
 
@@ -93,13 +93,7 @@ def main_window(frm_main):
         unit = event.widget.get()
 
         if unit == "Metric":
-            
-            ent_height2.config(width=0)
-            lbl_height2_unit.config(text="")
-
-            # lbl_height_unit = Label(frm_main, text="cm")
-            # lbl_height_unit.grid(row=3, column=2)
-            
+                        
             lbl_height_unit.config(text="cm")
             lbl_Weight_unit.config(text="kg")
 
@@ -115,8 +109,8 @@ def main_window(frm_main):
             # lbl_height2_unit = Label(frm_main, text="inches")
             # lbl_height2_unit.grid(row=3, column=4)
 
-            lbl_Weight_unit = Label(frm_main, text="pounds")
-            lbl_Weight_unit.grid(row=4, column=2)
+            lbl_height_unit.config(text="cm")
+            lbl_Weight_unit.config(text="pounds")
 
     btn_clear.config(command=clear)
     btn_calculate.config(command=calculate) 
