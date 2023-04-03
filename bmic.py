c7d32f11d7ae5b56ea052c601c4bf80afb8ace29
GPuebla <gabrielpuebla16@gmail.com>
2023-04-03 06:35:39 -0300

    update

--- bmic.py
+++ bmic.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter.ttk import *
-from tkinter import Frame, Label, Button
+from tkinter import Frame, Label, Button, IntVar,Text
 
 from number_entry import IntEntry, FloatEntry
 
@@ -42,7 +42,7 @@ def main_window(frm_main):
     comb.grid(row=0, column=1)
 
     # Creat a section witch show the leabel "Age" and get the years of the user.
-    lbl_age = Label(frm_main, text="year month:")
+    lbl_age = Label(frm_main, text="Birth year")
     lbl_age.grid(row=1, column=0)
     # ent_age = IntEntry(frm_main, width=15, lower_bound=2, upper_bound=120)
     # ent_age.grid(row=1, column=1)
@@ -54,17 +54,18 @@ def main_window(frm_main):
     comb_months["values"] = (' ','Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
     comb_months.grid(row=1, column=3)
 
+    # Creat a combobox that display the last 120 years.
     comb_years = Combobox(frm_main, width=4 )
-    comb_years["values"] = ("",1904, 1905, 1906, 1907, 1908, 1909, 1910, 1911, 1912, 1913, 1914, 1915, 1916, 1917, 1918, 1919, 1920, 1921, 1922, 1923, 1924, 1925, 1926, 1927, 1928, 1929, 1930, 1931, 1932, 1933, 1934, 1935, 1936, 1937, 1938, 1939, 1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023)
+    comb_years["values"] = ("",1904, 1905, 1906, 1907, 1908, 1909, 1910, 1911, 1912, 1913, 1914, 1915, 1916, 1917, 1918, 1919, 1920, 1921, 1922, 1923, 1924, 1925, 1926, 1927, 1928, 1929, 1930, 1931, 1932, 1933, 1934, 1935, 1936, 1937, 1938, 1939, 1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)
     comb_years.grid(row=1, column=1)
 
-
-    # Creat a section witch show the leabel "Gender" and get the gender of the user.
+    # Creat a section witch show the  "Gender" and get the gender of the user.
     lbl_gender = Label(frm_main, text="Gender" )
     lbl_gender.grid(row=2, column=0)
-    rad1 = Radiobutton(frm_main, text="Male", value= 1)
+    radio = IntVar()  
+    rad1 = Radiobutton(frm_main, variable= radio, text="Male", value= 1)
     rad1.grid(row=2, column=1)
-    rad2 = Radiobutton(frm_main, text="Famale", value= 2)
+    rad2 = Radiobutton(frm_main, variable= radio, text="Famale", value= 2)
     rad2.grid(row=2, column=2)
 
     lbl_height = Label(frm_main, text="Height" )
@@ -76,43 +77,76 @@ def main_window(frm_main):
 
     lbl_Weight = Label(frm_main, text="Weight" )
     lbl_Weight.grid(row=4, column=0)
-    ent_Weight = FloatEntry(frm_main, width=15, lower_bound=60, upper_bound=2000)
+    ent_Weight = FloatEntry(frm_main, width=15, lower_bound=0, upper_bound=400)
     ent_Weight.grid(row=4, column=1)
     lbl_Weight_unit = Label(frm_main, text="kg")
     lbl_Weight_unit.grid(row=4, column=2)
 
-    ent_height2 = FloatEntry(frm_main, width=7, lower_bound=0, upper_bound=200)
+    ent_height2 = FloatEntry(frm_main, width=7, lower_bound=0, upper_bound=300)
     lbl_height2_unit = Label(frm_main, text="inches")
 
-    lbl_result = Label(frm_main, text="----TEST_RESULT----")
+    lbl_result = Label(frm_main, text=" ")
     lbl_result.grid(row=5, column=1)
 
+    Output = Text(frm_main, height = 3, width = 25, bg = "light cyan")
+    Output.grid(row=6, column=1)
 
     btn_calculate = Button(frm_main, text="Calculate", background= "green")
-    btn_calculate.grid(row=6, column=1)
+    btn_calculate.grid(row=7, column=1)
+
     btn_clear = Button(frm_main, text="Clear", background= "gray")
-    btn_clear.grid(row=6, column=2)
+    btn_clear.grid(row=7, column=2)
 
     def calculate_BMI(height,weight):
 
         result = weight / (height/100)**2
+
         return result
     
     def convert_feet_inches_to_cm(feet, inches):
         result_cm = (feet * 0.3048 + inches * 0.0254)*100
+
         return result_cm
     
     def convert_pounds_to_kg(pounds):
         result_kg = pounds * 0.45359237
+
         return result_kg
     
     def calculate_total_months(birth_year,birth_month):
         current_month = datetime.today().month
         current_year = datetime.today().year
-
         total_months = (current_year - birth_year) * 12 - (birth_month - current_month )
 
         return total_months
+    
+    def calculate_BMI_status(months, gender, percentiles):
+
+        range1 = float(percentiles[1])
+        range2 = float(percentiles[2])
+        range3 = float(percentiles[3])
+        range4 = float(percentiles[4])
+        range5 = float(percentiles[5])
+        range6 = float(percentiles[6])
+        range7 = float(percentiles[7])
+        range8 = float(percentiles[8])
+        range9 = float(percentiles[9])
+        
+        status_1 = "Severe Thinness II"
+        status_2 = "Severe Thinness I"
+        status_3 = "Moderate Thinness"
+        status_4 = "Mild Thinness"
+        status_5 = "Normal"
+        status_6 = "Overweight"
+        status_7 = "Obese Class I"
+        status_8 = "Obese Class II"
+        status_9 = "Obese Class III"
+
+        if months > 61 and months <= 229:
+            return "TEENS-CHILDREN"
+        elif months > 229:
+            # usar los vlores de la pagina para establecer los status
+            return "ADULTS"        
     
     def clear():
         """Clear all the inputs and outputs."""
@@ -129,6 +163,8 @@ def main_window(frm_main):
         if units == "Metric":
             height = ent_height.get()
             weight = ent_Weight.get()
+
+
         elif units == "Us":
             feet = ent_height.get()
             inches = ent_height2.get()
@@ -140,10 +176,13 @@ def main_window(frm_main):
         m = comb_months.current()
         y = int(comb_years.get())
 
-        months = calculate_total_months(y,m)
+        t_months = calculate_total_months(y,m)
 
+        # msg = calculate_BMI_status(t_months,radio.get())
+        
         result = calculate_BMI(height, weight)
-        lbl_result.config(text= f'{result:0.2f} kg/m2' - months)
+        lbl_result.config(text= f'{result:0.2f} kg/m2 {t_months} ' )
+        Output.config(text= f'{result:0.2f} kg/m2 {t_months}' )
         
     def callbackFunc(event):
         unit = event.widget.get()
